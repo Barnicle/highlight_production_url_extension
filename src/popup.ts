1b68f6486ccace9e551be8edefb07eb6b64d4cd5
Evgenii Galimov <EvgeniiGalimov@hollandandbarrett.com>
2025-11-02 23:55:29 +0100

    add linter

--- src/popup.ts
+++ src/popup.ts
@@ -1,7 +1,9 @@
 const btn = document.getElementById('check') as HTMLButtonElement
 const result = document.getElementById('result') as HTMLElement
 
-btn.addEventListener('click', async () => {
+const onActiveTab = async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
   result.textContent = `Active tab URL: ${tab?.url ?? 'unknown'}`
-})
+}
+
+btn.addEventListener('click', onActiveTab)
